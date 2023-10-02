c9f4c7f480d86d1759346e1704eb4e5c57a5a84b
Guilherme Paixão <guiferpa@gmail.com>
2023-10-01 22:01:07 -0300

    wip: creating merkletree

---
+++ merkletree/merkletree.go
@@ -0,0 +1,35 @@
+package merkletree
+
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
+)
+
+func GenerateLeafHash(hl string, hr string) string {
+	payload := fmt.Sprintf("H::%s%s", hl, hr)
+	h := sha256.New()
+	h.Write([]byte(payload))
+	return hex.EncodeToString(h.Sum(nil))
+}
+
+func GenerateRootHash(hs []string) string {
+	if len(hs) == 1 {
+		return hs[0]
+	}
+	nhs := make([]string, 0)
+	i := 0
+	for i < len(hs) {
+		if i+1 < len(hs) {
+			l := hs[i]
+			r := hs[i+1]
+			nhs = append(nhs, GenerateLeafHash(l, r))
+			i += 2
+			continue
+		}
+		l := hs[i]
+		nhs = append(nhs, l)
+		i++
+	}
+	return GenerateRootHash(nhs)
+}
